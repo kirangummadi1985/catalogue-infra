a631dafc6fe947a9bf9cffb001babc870a025e74
kiran <kirangummadi1985@gmail.com>
2022-06-19 15:52:21 +0530

    Infra-setup

--- vars.tf
+++ vars.tf
@@ -11,3 +11,8 @@ variable "FAMILY" {}
 variable "NODE_TYPE" {}
 variable "NODE_COUNT" {}
 
+variable "EC2_NODE_TYPE" {}
+variable "PORT" {}
+variable "ONDEMAND_INSTANCE_COUNT" {}
+variable "SPOT_INSTANCE_COUNT" {}
+variable "WORKSTATION_IP" {}
