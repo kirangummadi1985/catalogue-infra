fee795cdac3a0ee4b17f9e6eafbe328a7160f5b2
kiran <kirangummadi1985@gmail.com>
2022-06-21 13:44:17 +0530

    Infra-setup

--- main.tf
+++ main.tf
@@ -39,4 +39,5 @@ module "app" {
   VPC_ACCESS_TO_ALB       = [module.vpc.VPC_CIDR]
   PRIVATE_HOSTED_ZONE_ID  = module.vpc.PRIVATE_HOSTED_ZONE_ID
   DOCDB_ENDPOINT          = module.documentdb.DOCDB_ENDPOINT
+  IAM_POLICY_CREATE       = true
 }
