08aeb397f76693b33494216eb6a3814bfe3b79b8
kiran <kirangummadi1985@gmail.com>
2022-06-21 14:04:42 +0530

    Infra-setup

--- vars.tf
+++ vars.tf
@@ -16,3 +16,10 @@ variable "PORT" {}
 variable "ONDEMAND_INSTANCE_COUNT" {}
 variable "SPOT_INSTANCE_COUNT" {}
 variable "WORKSTATION_IP" {}
+
+variable "DOCDB_ENGINE" {}
+variable "DOCDB_ENGINE_VERSION" {}
+variable "DOCDB_NODE_TYPE" {}
+variable "DOCDB_NODE_COUNT" {}
+variable "DOCDB_FAMILY" {}
+variable "DOCDB_SKIP_FINAL_SNAPSHOT" {}
